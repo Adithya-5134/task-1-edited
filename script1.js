67ee30a2b60d3243f09afaae2fc61a95c135c9ff
Adithya-5134 <125654086+Adithya-5134@users.noreply.github.com>
2023-04-06 22:22:59 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,49 @@
+const limenu=document.getElementById('menu');
+const icon=document.getElementById('butn');
+const home1=document.getElementById('home2');
+
+// MOBILE MENU//
+
+function mobilemenu(){
+    if(limenu.style.display=='block'){
+        limenu.style.display='none';
+        home1.style.marginTop="initial";
+        
+    }
+    else{
+        limenu.style.display='block';
+        home1.style.marginTop="280px";
+    }
+}
+
+icon.addEventListener('click',mobilemenu);
+
+
+//NORMAL MENU//
+
+function normalmenu(){
+    if(window.innerWidth>782){
+        limenu.style.display='inline'
+    }
+    else if(window.innerWidth <=782){
+        limenu.style.display='none';
+    }
+}
+
+window.addEventListener('resize',normalmenu);
+ 
+
+function loader(){
+    if(limenu.style.display=='block'){
+        limenu.style.display='none';
+        
+    }
+    else if( limenu.style.display='none'){
+        
+    }
+}
+
+window.addEventListener('load',loader)
+
+
+
